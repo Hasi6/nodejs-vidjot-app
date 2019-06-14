45fcaa015554c52c520bcbf1062a108f1fa4274e
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-06-14 18:51:04 +0530

    add ideas
    set server side form validations and now can add ideas and in ideas page they will display

--- Vidjoyt/app.js
+++ Vidjoyt/app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 
 const app = express();
+
+//Map globla promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+//connect to mongoose
+mongoose.connect("mongodb://localhost/project-ideas", { useNewUrlParser: true })
+    .then(() => { //promises is connected show this
+        console.log("Mongodb Connected");
+    })
+    .catch((err => { //is there is an error show the error
+        console.log(`Error Found ${err}`);
+    }));
+
+//Load Idea Models
+require("./models/Idea");
+const Idea = mongoose.model('Idea')
 
 //handlebars middleware
 app.engine('handlebars', handlebars({
@@ -9,6 +28,11 @@ app.engine('handlebars', handlebars({
 }));
 
 app.set('view engine', 'handlebars');
+
+// body parser middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 
 // index Route
@@ -25,6 +49,50 @@ app.get("/about", (req, res) => {
     res.render("About", {
         title: title
     });
+});
+
+//idea index page
+app.get("/ideas", (req, res) => {
+    Idea.find({})
+        .sort({ date: 'desc' })
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas: ideas
+            });
+        });
+});
+
+// add ideas form
+app.get("/ideas/add", (req, res) => {
+    res.render("ideas/ideasAdd");
+});
+
+// process Form
+app.post("/idea", (req, res) => {
+    let errors = [];
+    if (!req.body.title) {
+        errors.push({ text: "plese add a title" });
+    }
+    if (!req.body.details) {
+        errors.push({ text: "please add a description" });
+    }
+    if (errors.length > 0) {
+        res.render("ideas/ideasAdd", {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.title
+        });
+    } else {
+        const newIdea = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newIdea)
+            .save()
+            .then(idea => {
+                res.redirect("/ideas");
+            });
+    }
 });
 
 const port = 5500;
